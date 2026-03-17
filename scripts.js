dcd46001b8631ae6067f0c98141ef58a734c1c14
Felipe Nogueira <86392886+NOG01@users.noreply.github.com>
2026-03-16 21:54:40 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.addEventListener('click', () => {
-        document.body.classList.toggle('dark-theme');
-        document.body.classList.toggle('light-theme');
-    });
-});
